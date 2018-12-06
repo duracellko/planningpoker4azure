c1a900d1a3168c743f4ee9ccdba30ffe03649a0d
Rastislav Novotný <rasto@duracellko.net>
2018-12-06 20:19:51 +0800

    Extract JavaScript code to PlanningPoker.js

---
+++ src/Duracellko.PlanningPoker.Client/wwwroot/Scripts/PlanningPoker.js
@@ -0,0 +1,27 @@
+﻿var Duracellko;
+(function (Duracellko) {
+    // Static class providing functionality for Planning Poker application.
+    var PlanningPoker = (function () {
+        function PlanningPoker() {
+        }
+
+        // Shows message box using jQuery.
+        PlanningPoker.showMessageBox = function (element) {
+            $(element).modal({ backdrop: 'static' });
+        };
+
+        // Shows busy indicator using jQuery.
+        PlanningPoker.showBusyIndicator = function (element) {
+            $(element).modal({ backdrop: 'static', keyboard: false });
+        };
+
+        // Hides element using jQuery.
+        PlanningPoker.hide = function (element) {
+            $(element).modal('hide');
+        };
+
+        return PlanningPoker;
+    })();
+
+    Duracellko.PlanningPoker = PlanningPoker;
+})(Duracellko || (Duracellko = {}));
