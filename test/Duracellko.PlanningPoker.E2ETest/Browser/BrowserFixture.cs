ad076da0169b6aef1b5078883e7286b41984543c
Rastislav Novotný <rasto@duracellko.net>
2018-12-20 17:45:19 +0800

    Add Azure Pipeline

--- test/Duracellko.PlanningPoker.E2ETest/Browser/BrowserFixture.cs
+++ test/Duracellko.PlanningPoker.E2ETest/Browser/BrowserFixture.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Remote;
 
 namespace Duracellko.PlanningPoker.E2ETest.Browser
 {
@@ -28,8 +29,7 @@ namespace Duracellko.PlanningPoker.E2ETest.Browser
                 throw new InvalidOperationException("Selenium driver was already started.");
             }
 
-            var options = CreateDriverOptions(browserType);
-            var driver = new RemoteWebDriver(options);
+            var driver = CreateDriver(browserType);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
             Browser = driver;
         }
@@ -46,7 +46,7 @@ namespace Duracellko.PlanningPoker.E2ETest.Browser
             }
         }
 
-        private static DriverOptions CreateDriverOptions(BrowserType browserType)
+        private static IWebDriver CreateDriver(BrowserType browserType)
         {
             switch (browserType)
             {
@@ -55,16 +55,63 @@ namespace Duracellko.PlanningPoker.E2ETest.Browser
                     chromeOptions.AddArgument("--headless");
                     chromeOptions.AddArgument("--window-size=1920,1080");
                     chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
-                    return chromeOptions;
+                    return new ChromeDriver(GetDriverLocation(browserType), chromeOptions);
                 case BrowserType.Firefox:
                     var firefoxOptions = new FirefoxOptions();
                     firefoxOptions.AddArgument("-headless");
                     firefoxOptions.AddArgument("--window-size=1920,1080");
                     firefoxOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
-                    return firefoxOptions;
+                    return new FirefoxDriver(GetDriverLocation(browserType), firefoxOptions);
                 default:
                     throw new NotSupportedException($"Browser type '{browserType}' is not supported.");
             }
+        }
+
+        private static string GetDriverLocation(BrowserType browserType)
+        {
+            string driverName = null;
+            string environmentVariable = null;
+            switch (browserType)
+            {
+                case BrowserType.Chrome:
+                    driverName = "chromedriver";
+                    environmentVariable = "ChromeWebDriver";
+                    break;
+                case BrowserType.Firefox:
+                    driverName = "geckodriver";
+                    environmentVariable = "GeckoWebDriver";
+                    break;
+                default:
+                    throw new NotSupportedException($"Browser type '{browserType}' is not supported.");
+            }
+
+            var driverLocation = Environment.GetEnvironmentVariable(environmentVariable);
+            if (string.IsNullOrEmpty(driverLocation) || !Directory.Exists(driverLocation))
+            {
+                var assemblyLocation = Path.GetDirectoryName(typeof(BrowserFixture).Assembly.Location);
+                var seleniumFolder = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(assemblyLocation))));
+                seleniumFolder = Path.Combine(seleniumFolder, "node_modules", "selenium-standalone", ".selenium");
+                driverLocation = Path.Combine(seleniumFolder, driverName);
+
+                var driverFile = Directory.GetFiles(driverLocation)
+                    .Select(p => Path.GetFileName(p))
+                    .Where(f => !f.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
+                    .OrderByDescending(f => f, StringComparer.OrdinalIgnoreCase)
+                    .First();
+                driverFile = Path.Combine(driverLocation, driverFile);
+
+                var windir = Environment.GetEnvironmentVariable("windir");
+                var isWindows = !string.IsNullOrEmpty(windir) && Directory.Exists(windir);
+                var targetFile = isWindows ? (driverName + ".exe") : driverName;
+                targetFile = Path.Combine(driverLocation, targetFile);
+                if (!File.Exists(targetFile) || File.GetLastWriteTimeUtc(targetFile) != File.GetLastWriteTimeUtc(driverFile))
+                {
+                    File.Copy(driverFile, targetFile, true);
+                }
+            }
+
+            Console.WriteLine($"{browserType} location: {driverLocation}");
+            return driverLocation;
         }
     }
 }
