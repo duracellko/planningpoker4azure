2fc6d87cafd8fa78320e48fa160cce32a9a2b6ea
Rastislav Novotný <rasto@duracellko.net>
2020-03-15 12:55:12 +0800

    Use Development environment in E2E tests

--- test/Duracellko.PlanningPoker.E2ETest/Server/ServerFixture.cs
+++ test/Duracellko.PlanningPoker.E2ETest/Server/ServerFixture.cs
@@ -129,9 +129,11 @@ namespace Duracellko.PlanningPoker.E2ETest.Server
 
         private string[] GetProgramArguments()
         {
+            // Use Development environment, so that static web content is served without building project with publish.
             return new string[]
             {
                 "--urls", "http://127.0.0.1:0",
+                "--environment", "Development",
                 "--PlanningPokerClient:UseServerSide", UseServerSide.ToString(CultureInfo.InvariantCulture),
                 "--PlanningPokerClient:UseHttpClient", UseHttpClient.ToString(CultureInfo.InvariantCulture)
             };
