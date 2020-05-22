cad10536b161ca53ea1b48e600f7aa797ca4825b
Rastislav Novotný <rasto@duracellko.net>
2020-05-23 00:20:41 +0800

    Add rating deck and t-shirt sizes

--- src/Duracellko.PlanningPoker.Domain/DeckProvider.cs
+++ src/Duracellko.PlanningPoker.Domain/DeckProvider.cs
@@ -42,6 +42,29 @@ namespace Duracellko.PlanningPoker.Domain
             new Estimation()
         };
 
+        private readonly IEnumerable<Estimation> _ratingDeck = new Estimation[]
+        {
+            new Estimation(1.0),
+            new Estimation(2.0),
+            new Estimation(3.0),
+            new Estimation(4.0),
+            new Estimation(5.0),
+            new Estimation(6.0),
+            new Estimation(7.0),
+            new Estimation(8.0),
+            new Estimation(9.0),
+            new Estimation(10.0)
+        };
+
+        private readonly IEnumerable<Estimation> _tshirtSizes = new Estimation[]
+        {
+            new Estimation(-999509.0), // XS
+            new Estimation(-999508.0), // S
+            new Estimation(-999507.0), // M
+            new Estimation(-999506.0), // L
+            new Estimation(-999505.0), // XL
+        };
+
         private readonly IEnumerable<Estimation> _rockPaperScissorsLizardSpock = new Estimation[]
         {
             new Estimation(-999909.0), // Rock
@@ -70,6 +93,10 @@ namespace Duracellko.PlanningPoker.Domain
                     return _standardDeck;
                 case Deck.Fibonacci:
                     return _fibonacciDeck;
+                case Deck.Rating:
+                    return _ratingDeck;
+                case Deck.Tshirt:
+                    return _tshirtSizes;
                 case Deck.RockPaperScissorsLizardSpock:
                     return _rockPaperScissorsLizardSpock;
                 default:
