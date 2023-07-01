85653941d7c315027dba4a8b057ed361f211c6b8
Rastislav Novotný <rasto@duracellko.net>
2023-07-01 22:52:38 +0800

    Update message in E2E test

--- test/Duracellko.PlanningPoker.E2ETest/ScrumMasterTest.cs
+++ test/Duracellko.PlanningPoker.E2ETest/ScrumMasterTest.cs
@@ -160,7 +160,7 @@ namespace Duracellko.PlanningPoker.E2ETest
             await Task.Delay(500);
             ClientTest.AssertIndexPage();
             TakeScreenshot("04-Error");
-            ClientTest.AssertMessageBox("Scrum Team \"My team\" does not exist. (Parameter 'teamName')");
+            ClientTest.AssertMessageBox("Scrum Team 'My team' does not exist.");
         }
 
         [DataTestMethod]
