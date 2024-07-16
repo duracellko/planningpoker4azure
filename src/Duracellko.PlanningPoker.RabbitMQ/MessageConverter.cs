8a8aa302baa0782cdce4003bf60abfb15abd3f57
Rastislav Novotný <rasto@duracellko.net>
2024-07-16 23:10:24 +0800

    RabbitMQ support

---
+++ src/Duracellko.PlanningPoker.RabbitMQ/MessageConverter.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Duracellko.PlanningPoker.Azure;
+
+namespace Duracellko.PlanningPoker.RabbitMQ;
+
+/// <summary>
+/// Instance of this class is able to convert messages of type <see cref="T:NodeMessage"/> to RabbitMQ message and vice versa.
+/// </summary>
+public class MessageConverter : IMessageConverter
+{
+    /// <summary>
+    /// Name of property in Rabbit MQ message holding recipient node ID.
+    /// </summary>
+    internal const string RecipientIdPropertyName = PropertyPrefix + "RecipientId";
+
+    /// <summary>
+    /// Name of property in Rabbit MQ message holding sender node ID.
+    /// </summary>
+    internal const string SenderIdPropertyName = PropertyPrefix + "SenderId";
+
+    private const string PropertyPrefix = "PlanningPoker-";
+    private const string MessageTypePropertyName = PropertyPrefix + "MessageType";
+    private const string MessageSubtypePropertyName = PropertyPrefix + "MessageSubtype";
+
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
+    };
+
+    private static readonly Encoding _headersEncoding = Encoding.UTF8;
+
+    /// <summary>
+    /// Gets headers of RabbitMQ message converted from <see cref="T:NodeMessage"/>.
+    /// </summary>
+    /// <param name="message">The message to convert.</param>
+    /// <returns>Headers of the message.</returns>
+    public IDictionary<string, object> GetMessageHeaders(NodeMessage message)
+    {
+        ArgumentNullException.ThrowIfNull(message);
+
+        var headers = new Dictionary<string, object>();
+        SetHeader(headers, MessageTypePropertyName, message.MessageType.ToString());
+        if (message.Data != null)
+        {
+            SetHeader(headers, MessageSubtypePropertyName, message.Data.GetType().Name);
+        }
+
+        SetHeader(headers, SenderIdPropertyName, message.SenderNodeId);
+        SetHeader(headers, RecipientIdPropertyName, message.RecipientNodeId);
+
+        return headers;
+    }
+
+    /// <summary>
+    /// Gets body of RabbitMQ message converted from <see cref="T:NodeMessage"/>.
+    /// </summary>
+    /// <param name="message">The message to convert.</param>
+    /// <returns>Body of the message.</returns>
+    public ReadOnlyMemory<byte> GetMessageBody(NodeMessage message)
+    {
+        ArgumentNullException.ThrowIfNull(message);
+
+        if (message.MessageType == NodeMessageType.InitializeTeam || message.MessageType == NodeMessageType.TeamCreated)
+        {
+            return ConvertToMessageBody((byte[])message.Data!);
+        }
+        else if (message.Data != null)
+        {
+            return ConvertToMessageBody(message.Data);
+        }
+        else
+        {
+            return Array.Empty<byte>();
+        }
+    }
+
+    /// <summary>
+    /// Converts RabbitMQ message headers and body to <see cref="T:NodeMessage"/> object.
+    /// </summary>
+    /// <param name="headers">Headers of the message to convert.</param>
+    /// <param name="body">Body of the message to convert.</param>
+    /// <returns>Converted message of NodeMessage type.</returns>
+    public NodeMessage GetNodeMessage(IDictionary<string, object> headers, ReadOnlyMemory<byte> body)
+    {
+        ArgumentNullException.ThrowIfNull(headers);
+
+        var messageTypeValue = GetHeader(headers, MessageTypePropertyName);
+        var messageType = (NodeMessageType)Enum.Parse(typeof(NodeMessageType), messageTypeValue!);
+        var messageSubtype = GetHeader(headers, MessageSubtypePropertyName);
+
+        var result = new NodeMessage(messageType);
+        result.SenderNodeId = GetHeader(headers, SenderIdPropertyName);
+        result.RecipientNodeId = GetHeader(headers, RecipientIdPropertyName);
+
+        switch (result.MessageType)
+        {
+            case NodeMessageType.ScrumTeamMessage:
+                if (string.Equals(messageSubtype, typeof(ScrumTeamMemberMessage).Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Data = ConvertFromMessageBody<ScrumTeamMemberMessage>(body);
+                }
+                else if (string.Equals(messageSubtype, typeof(ScrumTeamMemberEstimationMessage).Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Data = ConvertFromMessageBody<ScrumTeamMemberEstimationMessage>(body);
+                }
+                else if (string.Equals(messageSubtype, typeof(ScrumTeamEstimationSetMessage).Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Data = ConvertFromMessageBody<ScrumTeamEstimationSetMessage>(body);
+                }
+                else if (string.Equals(messageSubtype, typeof(ScrumTeamTimerMessage).Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Data = ConvertFromMessageBody<ScrumTeamTimerMessage>(body);
+                }
+                else
+                {
+                    result.Data = ConvertFromMessageBody<ScrumTeamMessage>(body);
+                }
+
+                break;
+            case NodeMessageType.TeamCreated:
+            case NodeMessageType.InitializeTeam:
+                result.Data = ConvertFromMessageBody(body);
+                break;
+            case NodeMessageType.TeamList:
+            case NodeMessageType.RequestTeams:
+                result.Data = ConvertFromMessageBody<string[]>(body);
+                break;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Gets decoded value of Rabbit MQ message header with specified key.
+    /// </summary>
+    /// <param name="headers">The collection of header key-value pairs.</param>
+    /// <param name="key">The key to get header value for.</param>
+    /// <returns>Value header with specified key.</returns>
+    public string? GetHeader(IDictionary<string, object> headers, string key)
+    {
+        ArgumentNullException.ThrowIfNull(headers);
+        ArgumentNullException.ThrowIfNullOrEmpty(key);
+
+        if (headers.TryGetValue(key, out var valueObject) && valueObject != null)
+        {
+            return _headersEncoding.GetString((byte[])valueObject);
+        }
+
+        return null;
+    }
+
+    private static ReadOnlyMemory<byte> ConvertToMessageBody(object data)
+    {
+        return JsonSerializer.SerializeToUtf8Bytes(data, data.GetType(), _jsonSerializerOptions);
+    }
+
+    private static ReadOnlyMemory<byte> ConvertToMessageBody(byte[] data)
+    {
+        using (var dataStream = new MemoryStream())
+        {
+            using (var deflateStream = new DeflateStream(dataStream, CompressionMode.Compress, true))
+            {
+                deflateStream.Write(data, 0, data.Length);
+                deflateStream.Flush();
+            }
+
+            return dataStream.ToArray();
+        }
+    }
+
+    private static T? ConvertFromMessageBody<T>(ReadOnlyMemory<byte> body)
+    {
+        return JsonSerializer.Deserialize<T>(body.Span, _jsonSerializerOptions);
+    }
+
+    private static byte[] ConvertFromMessageBody(ReadOnlyMemory<byte> body)
+    {
+        using (var dataStream = new MemoryStream(body.ToArray()))
+        {
+            using (var deflateStream = new DeflateStream(dataStream, CompressionMode.Decompress))
+            {
+                using var memoryStream = new MemoryStream();
+                deflateStream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    private static void SetHeader(IDictionary<string, object> headers, string key, string? value)
+    {
+        if (value != null)
+        {
+            headers[key] = _headersEncoding.GetBytes(value);
+        }
+    }
+}
---
+++ test/Duracellko.PlanningPoker.RabbitMQ.Test/MessageConverterTest.cs
@@ -0,0 +1,260 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using Duracellko.PlanningPoker.Azure;
+using Duracellko.PlanningPoker.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Duracellko.PlanningPoker.RabbitMQ.Test;
+
+[TestClass]
+public class MessageConverterTest
+{
+    private const string SenderId = "3d1c7636-ae1d-4288-b1e1-0dccc8989722";
+    private const string RecipientId = "10243241-802e-4d66-b4fc-55c76c23bcb2";
+    private const string TeamName = "My Team";
+    private const string Team1Json = "{\"Name\":\"My Team\",\"State\":1,\"AvailableEstimations\":[{\"Value\":0.0},{\"Value\":0.5},{\"Value\":1.0},{\"Value\":2.0},{\"Value\":3.0},{\"Value\":5.0},{\"Value\":8.0},{\"Value\":13.0},{\"Value\":20.0},{\"Value\":40.0},{\"Value\":100.0},{\"Value\":\"Infinity\"},{\"Value\":null}],\"Members\":[{\"Name\":\"Duracellko\",\"MemberType\":2,\"Messages\":[],\"LastMessageId\":3,\"LastActivity\":\"2020-05-24T14:46:48.1509407Z\",\"IsDormant\":false,\"Estimation\":null},{\"Name\":\"Me\",\"MemberType\":1,\"Messages\":[],\"LastMessageId\":2,\"LastActivity\":\"2020-05-24T14:47:40.119354Z\",\"IsDormant\":false,\"Estimation\":{\"Value\":20.0}}],\"EstimationResult\":{\"Duracellko\":null,\"Me\":{\"Value\":20.0}}}";
+    private const string Team2Json = "{\"Name\":\"My Team\",\"State\":1,\"AvailableEstimations\":[{\"Value\":0.0},{\"Value\":0.5},{\"Value\":1.0},{\"Value\":2.0},{\"Value\":3.0},{\"Value\":5.0},{\"Value\":8.0},{\"Value\":13.0},{\"Value\":20.0},{\"Value\":40.0},{\"Value\":100.0},{\"Value\":\"Infinity\"},{\"Value\":null}],\"Members\":[{\"Name\":\"Duracellko\",\"MemberType\":2,\"Messages\":[],\"LastMessageId\":9,\"LastActivity\":\"2020-05-24T14:53:07.6381166Z\",\"IsDormant\":false,\"Estimation\":{\"Value\":2.0}},{\"Name\":\"Me\",\"MemberType\":1,\"Messages\":[],\"LastMessageId\":8,\"LastActivity\":\"2020-05-24T14:53:05.8193334Z\",\"IsDormant\":false,\"Estimation\":{\"Value\":5.0}},{\"Name\":\"Test\",\"MemberType\":1,\"Messages\":[{\"Id\":4,\"MessageType\":6,\"MemberName\":\"Duracellko\",\"EstimationResult\":null},{\"Id\":5,\"MessageType\":6,\"MemberName\":\"Me\",\"EstimationResult\":null}],\"LastMessageId\":5,\"LastActivity\":\"2020-05-24T14:52:40.0708949Z\",\"IsDormant\":false,\"Estimation\":null}],\"EstimationResult\":{\"Duracellko\":{\"Value\":2.0},\"Me\":{\"Value\":5.0},\"Test\":null}}";
+
+    [TestMethod]
+    public void GetHeaders_Null_ArgumentNullException()
+    {
+        var target = new MessageConverter();
+        Assert.ThrowsException<ArgumentNullException>(() => target.GetMessageHeaders(null!));
+    }
+
+    [TestMethod]
+    public void GetMessageBody_Null_ArgumentNullException()
+    {
+        var target = new MessageConverter();
+        Assert.ThrowsException<ArgumentNullException>(() => target.GetMessageBody(null!));
+    }
+
+    [TestMethod]
+    public void GetNodeMessage_Null_ArgumentNullException()
+    {
+        var target = new MessageConverter();
+        Assert.ThrowsException<ArgumentNullException>(() => target.GetNodeMessage(null!, null));
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_ScrumTeamMessage()
+    {
+        var scrumTeamMessage = new ScrumTeamMessage(TeamName, MessageType.EstimationStarted);
+        var nodeMessage = new NodeMessage(NodeMessageType.ScrumTeamMessage)
+        {
+            SenderNodeId = SenderId,
+            Data = scrumTeamMessage
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+        var resultData = (ScrumTeamMessage)result.Data!;
+
+        Assert.AreEqual(MessageType.EstimationStarted, resultData.MessageType);
+        Assert.AreEqual(TeamName, resultData.TeamName);
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_ScrumTeamMemberMessage()
+    {
+        var scrumTeamMessage = new ScrumTeamMemberMessage(TeamName, MessageType.MemberJoined)
+        {
+            MemberType = "Observer",
+            MemberName = "Test person",
+            SessionId = Guid.NewGuid()
+        };
+        var nodeMessage = new NodeMessage(NodeMessageType.ScrumTeamMessage)
+        {
+            SenderNodeId = SenderId,
+            Data = scrumTeamMessage
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+        var resultData = (ScrumTeamMemberMessage)result.Data!;
+
+        Assert.AreEqual(MessageType.MemberJoined, resultData.MessageType);
+        Assert.AreEqual(TeamName, resultData.TeamName);
+        Assert.AreEqual(scrumTeamMessage.MemberType, resultData.MemberType);
+        Assert.AreEqual(scrumTeamMessage.MemberName, resultData.MemberName);
+        Assert.AreEqual(scrumTeamMessage.SessionId, resultData.SessionId);
+    }
+
+    [DataTestMethod]
+    [DataRow(8.0)]
+    [DataRow(0.5)]
+    [DataRow(0.0)]
+    [DataRow(null)]
+    [DataRow(double.PositiveInfinity)]
+    public void ConvertToRabbitMQMessageAndBack_ScrumTeamMemberEstimationMessage(double? estimation)
+    {
+        var scrumTeamMessage = new ScrumTeamMemberEstimationMessage(TeamName, MessageType.MemberEstimated)
+        {
+            MemberName = "Scrum Master",
+            Estimation = estimation
+        };
+        var nodeMessage = new NodeMessage(NodeMessageType.ScrumTeamMessage)
+        {
+            SenderNodeId = SenderId,
+            Data = scrumTeamMessage
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+        var resultData = (ScrumTeamMemberEstimationMessage)result.Data!;
+
+        Assert.AreEqual(MessageType.MemberEstimated, resultData.MessageType);
+        Assert.AreEqual(TeamName, resultData.TeamName);
+        Assert.AreEqual(scrumTeamMessage.MemberName, resultData.MemberName);
+        Assert.AreEqual(scrumTeamMessage.Estimation, resultData.Estimation);
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_ScrumTeamEstimationSetMessage()
+    {
+        var deck = DeckProvider.Default.GetDefaultDeck().Select(e => e.Value).ToList();
+        var scrumTeamMessage = new ScrumTeamEstimationSetMessage(TeamName, MessageType.AvailableEstimationsChanged)
+        {
+            Estimations = deck
+        };
+        var nodeMessage = new NodeMessage(NodeMessageType.ScrumTeamMessage)
+        {
+            SenderNodeId = SenderId,
+            Data = scrumTeamMessage
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+        var resultData = (ScrumTeamEstimationSetMessage)result.Data!;
+
+        Assert.AreEqual(MessageType.AvailableEstimationsChanged, resultData.MessageType);
+        Assert.AreEqual(TeamName, resultData.TeamName);
+        CollectionAssert.AreEqual(deck, resultData.Estimations.ToList());
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_ScrumTeamTimerMessage()
+    {
+        var scrumTeamMessage = new ScrumTeamTimerMessage(TeamName, MessageType.TimerStarted)
+        {
+            EndTime = new DateTime(2021, 11, 16, 23, 49, 31, DateTimeKind.Utc)
+        };
+        var nodeMessage = new NodeMessage(NodeMessageType.ScrumTeamMessage)
+        {
+            SenderNodeId = SenderId,
+            Data = scrumTeamMessage
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+        var resultData = (ScrumTeamTimerMessage)result.Data!;
+
+        Assert.AreEqual(MessageType.TimerStarted, resultData.MessageType);
+        Assert.AreEqual(TeamName, resultData.TeamName);
+        Assert.AreEqual(scrumTeamMessage.EndTime, resultData.EndTime);
+        Assert.AreEqual(DateTimeKind.Utc, resultData.EndTime.Kind);
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_TeamCreated()
+    {
+        var nodeMessage = new NodeMessage(NodeMessageType.TeamCreated)
+        {
+            SenderNodeId = SenderId,
+            Data = Encoding.UTF8.GetBytes(Team1Json)
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+
+        var resultJson = Encoding.UTF8.GetString((byte[])result.Data!);
+        Assert.AreEqual(Team1Json, resultJson);
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_RequestTeamList()
+    {
+        var nodeMessage = new NodeMessage(NodeMessageType.RequestTeamList)
+        {
+            SenderNodeId = SenderId
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1011:Closing square brackets should be spaced correctly", Justification = "Nullable array")]
+    public void ConvertToRabbitMQMessageAndBack_TeamList()
+    {
+        var teamList = new[] { TeamName, "Test", "Hello, World!" };
+        var nodeMessage = new NodeMessage(NodeMessageType.TeamList)
+        {
+            SenderNodeId = SenderId,
+            RecipientNodeId = RecipientId,
+            Data = teamList
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+
+        CollectionAssert.AreEqual(teamList, (string[]?)result.Data);
+    }
+
+    [TestMethod]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1011:Closing square brackets should be spaced correctly", Justification = "Nullable array")]
+    public void ConvertToRabbitMQMessageAndBack_RequestTeams()
+    {
+        var teamList = new[] { TeamName };
+        var nodeMessage = new NodeMessage(NodeMessageType.RequestTeams)
+        {
+            SenderNodeId = SenderId,
+            RecipientNodeId = RecipientId,
+            Data = teamList
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+
+        CollectionAssert.AreEqual(teamList, (string[]?)result.Data);
+    }
+
+    [TestMethod]
+    public void ConvertToRabbitMQMessageAndBack_InitializeTeam()
+    {
+        var nodeMessage = new NodeMessage(NodeMessageType.InitializeTeam)
+        {
+            SenderNodeId = SenderId,
+            RecipientNodeId = RecipientId,
+            Data = Encoding.UTF8.GetBytes(Team2Json)
+        };
+
+        var result = ConvertToRabbitMQMessageAndBack(nodeMessage);
+
+        var resultJson = Encoding.UTF8.GetString((byte[])result.Data!);
+        Assert.AreEqual(Team2Json, resultJson);
+    }
+
+    private static NodeMessage ConvertToRabbitMQMessageAndBack(NodeMessage nodeMessage)
+    {
+        var target = new MessageConverter();
+
+        var headers = target.GetMessageHeaders(nodeMessage);
+        var body = target.GetMessageBody(nodeMessage);
+
+        var result = target.GetNodeMessage(headers, body);
+
+        Assert.IsNotNull(result);
+        Assert.AreNotSame(nodeMessage, result);
+        Assert.AreEqual(nodeMessage.MessageType, result.MessageType);
+        Assert.AreEqual(nodeMessage.SenderNodeId, result.SenderNodeId);
+        Assert.AreEqual(nodeMessage.RecipientNodeId, result.RecipientNodeId);
+
+        if (nodeMessage.Data == null)
+        {
+            Assert.IsNull(result.Data);
+        }
+        else
+        {
+            Assert.IsNotNull(result.Data);
+            Assert.AreEqual(nodeMessage.Data.GetType(), result.Data.GetType());
+        }
+
+        return result;
+    }
+}
