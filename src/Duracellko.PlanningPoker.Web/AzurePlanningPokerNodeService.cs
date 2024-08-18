f8b38a6c381543301e48184a76077e6d85015768
Rastislav Novotný <rasto@duracellko.net>
2024-08-18 22:12:59 +0800

    Docker container tests with RabbitMQ

--- src/Duracellko.PlanningPoker.Web/AzurePlanningPokerNodeService.cs
+++ src/Duracellko.PlanningPoker.Web/AzurePlanningPokerNodeService.cs
@@ -1,24 +1,40 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Duracellko.PlanningPoker.Azure;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Duracellko.PlanningPoker.Web;
 
 public sealed class AzurePlanningPokerNodeService : IHostedService, IDisposable
 {
-    private readonly PlanningPokerAzureNode _node;
+    private static readonly Action<ILogger, Exception?> _logErrorStart = LoggerMessage.Define(
+        LogLevel.Error,
+        new EventId(0, "ErrorStartAzurePlanningPokerNode"),
+        "Starting Azure PlanningPoker Node failed.");
 
-    public AzurePlanningPokerNodeService(PlanningPokerAzureNode node)
+    private readonly PlanningPokerAzureNode _node;
+    private readonly ILogger _logger;
+
+    public AzurePlanningPokerNodeService(PlanningPokerAzureNode node, ILogger<AzurePlanningPokerNodeService> logger)
     {
         _node = node ?? throw new ArgumentNullException(nameof(node));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Log error and continue without Azure node service.")]
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        _node.Start();
-        return Task.CompletedTask;
+        try
+        {
+            await _node.Start();
+        }
+        catch (Exception ex)
+        {
+            _logErrorStart(_logger, ex);
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
