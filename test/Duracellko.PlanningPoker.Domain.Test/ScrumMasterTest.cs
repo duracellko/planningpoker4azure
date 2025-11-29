f976654345a38c1c0b040728f988ea8f6625e2da
Rastislav Novotný <rasto@duracellko.net>
2025-11-29 22:54:08 +0800

    ScrumMaster Close Estimation operation

--- test/Duracellko.PlanningPoker.Domain.Test/ScrumMasterTest.cs
+++ test/Duracellko.PlanningPoker.Domain.Test/ScrumMasterTest.cs
@@ -551,6 +551,340 @@ public class ScrumMasterTest
     }
 
     [TestMethod]
+    public void CloseEstimation_2MembersWithoutVote_MemberEstimationsAreSetToNullEstimation()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var noVote1 = (Member)team.Join("no vote 1", false);
+        var member = (Member)team.Join("member", false);
+        var noVote2 = (Member)team.Join("no vote 2", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(EstimationTestData.Unknown);
+        master.Estimation = new Estimation(0);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(noVote1.Estimation);
+        Assert.IsNull(noVote1.Estimation.Value);
+        Assert.IsNotNull(noVote2.Estimation);
+        Assert.IsNull(noVote2.Estimation.Value);
+        Assert.IsNotNull(master.Estimation);
+        Assert.AreEqual<double?>(0, master.Estimation.Value);
+        Assert.IsNotNull(member.Estimation);
+        Assert.AreEqual<double?>(EstimationTestData.Unknown, member.Estimation.Value);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_NoVotes_MemberEstimationsAreSetToNullEstimation()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var member = (Member)team.Join("member", false);
+        master.StartEstimation();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(master.Estimation);
+        Assert.IsNull(master.Estimation.Value);
+        Assert.IsNotNull(member.Estimation);
+        Assert.IsNull(member.Estimation.Value);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_NoVotes_StateChangedToEstimationFinished()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        team.Join("member", false);
+        team.Join("no vote", false);
+        master.StartEstimation();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.AreEqual<TeamState>(TeamState.EstimationFinished, team.State);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_2MembersWithoutVote_EstimationResultIsGenerated()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var noVote1 = (Member)team.Join("no vote 1", false);
+        var member = (Member)team.Join("member", false);
+        var noVote2 = (Member)team.Join("no vote 2", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(0.5);
+        master.Estimation = new Estimation(EstimationTestData.Infinity);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(team.EstimationResult);
+        var expectedResult = new KeyValuePair<Member, Estimation>[]
+        {
+            new(master, new Estimation(EstimationTestData.Infinity)),
+            new(member, new Estimation(0.5)),
+            new(noVote1, new Estimation()),
+            new(noVote2, new Estimation())
+        };
+        CollectionAssert.AreEquivalent(expectedResult, team.EstimationResult.ToList());
+    }
+
+    [TestMethod]
+    public void CloseEstimation_NoVotes_EstimationResultIsGenerated()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        master.StartEstimation();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(team.EstimationResult);
+        Assert.HasCount(1, team.EstimationResult);
+        Assert.AreEqual<Estimation?>(new Estimation(), team.EstimationResult[master]);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_2MembersWithoutVote_ScrumTeamGetEstimationEndedMessage()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var noVote1 = (Member)team.Join("no vote 1", false);
+        var member = (Member)team.Join("member", false);
+        var noVote2 = (Member)team.Join("no vote 2", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(EstimationTestData.Unknown);
+        master.Estimation = new Estimation(0);
+
+        var eventArgsList = new List<MessageReceivedEventArgs>();
+        team.MessageReceived += new EventHandler<MessageReceivedEventArgs>((s, e) => eventArgsList.Add(e));
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.HasCount(3, eventArgsList);
+        var message1 = eventArgsList[0].Message;
+        var message2 = eventArgsList[1].Message;
+        var message3 = eventArgsList[2].Message;
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message1.MessageType);
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message2.MessageType);
+        var memberMessage1 = Assert.IsInstanceOfType<MemberMessage>(message1);
+        var memberMessage2 = Assert.IsInstanceOfType<MemberMessage>(message2);
+
+        var expectedMessageMembers = new Observer[] { noVote1, noVote2 };
+        var actualMessageMembers = new Observer[] { memberMessage1.Member, memberMessage2.Member };
+        CollectionAssert.AreEquivalent(expectedMessageMembers, actualMessageMembers);
+
+        Assert.AreEqual<MessageType>(MessageType.EstimationEnded, message3.MessageType);
+        var estimationEndedMessage = Assert.IsInstanceOfType<EstimationResultMessage>(message3);
+        var expectedResult = new KeyValuePair<Member, Estimation>[]
+        {
+            new(master, new Estimation(0)),
+            new(member, new Estimation(EstimationTestData.Unknown)),
+            new(noVote1, new Estimation()),
+            new(noVote2, new Estimation())
+        };
+        CollectionAssert.AreEquivalent(expectedResult, estimationEndedMessage.EstimationResult.ToList());
+    }
+
+    [TestMethod]
+    public void CloseEstimation_ScrumMasterWithoutVote_ScrumMasterGetEstimationEndedMessage()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        master.StartEstimation();
+
+        master.ClearMessages();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        var masterMessages = master.Messages.ToList();
+        Assert.HasCount(2, masterMessages);
+        var message1 = masterMessages[0];
+        var message2 = masterMessages[1];
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message1.MessageType);
+        var memberMessage1 = Assert.IsInstanceOfType<MemberMessage>(message1);
+        Assert.AreEqual<Observer>(master, memberMessage1.Member);
+
+        Assert.AreEqual<MessageType>(MessageType.EstimationEnded, message2.MessageType);
+        var estimationEndedMessage = Assert.IsInstanceOfType<EstimationResultMessage>(message2);
+        Assert.HasCount(1, estimationEndedMessage.EstimationResult);
+        Assert.AreEqual<Estimation?>(new Estimation(), estimationEndedMessage.EstimationResult[master]);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_2MembersWithoutVote_MemberGetEstimationEndedMessage()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var noVote1 = (Member)team.Join("no vote 1", false);
+        var member = (Member)team.Join("member", false);
+        var noVote2 = (Member)team.Join("no vote 2", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(EstimationTestData.Unknown);
+        master.Estimation = new Estimation(0);
+
+        member.ClearMessages();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        var memberMessages = member.Messages.ToList();
+        Assert.HasCount(3, memberMessages);
+        var message1 = memberMessages[0];
+        var message2 = memberMessages[1];
+        var message3 = memberMessages[2];
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message1.MessageType);
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message2.MessageType);
+        var memberMessage1 = Assert.IsInstanceOfType<MemberMessage>(message1);
+        var memberMessage2 = Assert.IsInstanceOfType<MemberMessage>(message2);
+
+        var expectedMessageMembers = new Observer[] { noVote1, noVote2 };
+        var actualMessageMembers = new Observer[] { memberMessage1.Member, memberMessage2.Member };
+        CollectionAssert.AreEquivalent(expectedMessageMembers, actualMessageMembers);
+
+        Assert.AreEqual<MessageType>(MessageType.EstimationEnded, message3.MessageType);
+        var estimationEndedMessage = Assert.IsInstanceOfType<EstimationResultMessage>(message3);
+        var expectedResult = new KeyValuePair<Member, Estimation>[]
+        {
+            new(master, new Estimation(0)),
+            new(member, new Estimation(EstimationTestData.Unknown)),
+            new(noVote1, new Estimation()),
+            new(noVote2, new Estimation())
+        };
+        CollectionAssert.AreEquivalent(expectedResult, estimationEndedMessage.EstimationResult.ToList());
+    }
+
+    [TestMethod]
+    public void CloseEstimation_MemberWithoutVote_MemberGetEstimationEndedMessage()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var member = (Member)team.Join("member", false);
+        var noVote = (Member)team.Join("no vote", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(8);
+        master.Estimation = new Estimation(3);
+
+        noVote.ClearMessages();
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        var memberMessages = noVote.Messages.ToList();
+        Assert.HasCount(2, memberMessages);
+        var message1 = memberMessages[0];
+        var message2 = memberMessages[1];
+        Assert.AreEqual<MessageType>(MessageType.MemberEstimated, message1.MessageType);
+        var memberMessage1 = Assert.IsInstanceOfType<MemberMessage>(message1);
+        Assert.AreEqual<Observer>(noVote, memberMessage1.Member);
+
+        Assert.AreEqual<MessageType>(MessageType.EstimationEnded, message2.MessageType);
+        var estimationEndedMessage = Assert.IsInstanceOfType<EstimationResultMessage>(message2);
+        var expectedResult = new KeyValuePair<Member, Estimation>[]
+        {
+            new(master, new Estimation(3)),
+            new(member, new Estimation(8)),
+            new(noVote, new Estimation())
+        };
+        CollectionAssert.AreEquivalent(expectedResult, estimationEndedMessage.EstimationResult.ToList());
+    }
+
+    [TestMethod]
+    public void CloseEstimation_NoVotes_ScrumMasterMessageReceived()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        master.StartEstimation();
+
+        EventArgs? eventArgs = null;
+        master.MessageReceived += new EventHandler((s, e) => eventArgs = e);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(eventArgs);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_MemberWithoutVote_MemberMessageReceived()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var member = (Member)team.Join("member", false);
+        var noVote = (Member)team.Join("no vote", false);
+        master.StartEstimation();
+        member.Estimation = new Estimation(8);
+        master.Estimation = new Estimation(3);
+
+        EventArgs? eventArgs = null;
+        noVote.MessageReceived += new EventHandler((s, e) => eventArgs = e);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNotNull(eventArgs);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_InitialState_StateIsNotChanged()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        team.Join("member", false);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.AreEqual<TeamState>(TeamState.Initial, team.State);
+    }
+
+    [TestMethod]
+    public void CloseEstimation_InitialState_EstimationsAreNotChanged()
+    {
+        // Arrange
+        var team = new ScrumTeam("test team");
+        var master = team.SetScrumMaster("master");
+        var member = (Member)team.Join("member", false);
+
+        // Act
+        master.CloseEstimation();
+
+        // Verify
+        Assert.IsNull(master.Estimation);
+        Assert.IsNull(member.Estimation);
+    }
+
+    [TestMethod]
     public void UpdateActivity_IsDormant_IsNotDormant()
     {
         // Arrange
